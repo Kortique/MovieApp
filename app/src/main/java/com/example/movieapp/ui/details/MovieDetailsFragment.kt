6595e12aaa4050fcc68a8e287a62ddc04c66c51b
Kortique <40215395+Kortique@users.noreply.github.com>
2021-09-09 12:26:28 +0300

    Made some improvements

--- app/src/main/java/com/example/movieapp/ui/details/MovieDetailsFragment.kt
+++ app/src/main/java/com/example/movieapp/ui/details/MovieDetailsFragment.kt
@@ -24,13 +24,12 @@ class MovieDetailsFragment : Fragment() {
     private var _binding: FragmentMovieDetailsBinding? = null
     private val binding get() = _binding!!
 
-    private lateinit var movie: Movie
+    private var movie: Movie? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         arguments?.let {
-            val movieTmp = it.getParcelable(MOVIE_ARG) as Movie?
-            movie = movieTmp!!
+            movie = it.getParcelable(MOVIE_ARG) as? Movie
         }
     }
 
@@ -41,14 +40,15 @@ class MovieDetailsFragment : Fragment() {
         _binding = FragmentMovieDetailsBinding.inflate(inflater, container, false)
         return binding.root
     }
-
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
-        with(binding) {
-            movieTitle.text = movie.title
-            movieReleaseDate.text = movie.releaseDate.toString("yyyy-MM-dd")
-            movieOverview.text = movie.overview
+        movie?.let {
+            with(binding) {
+                movieTitle.text = it.title
+                movieReleaseDate.text = it.releaseDate.toString("yyyy-MM-dd")
+                movieOverview.text = it.overview
+            }
         }
     }
 
