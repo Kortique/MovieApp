0359c2f1216a7b2bb41a7b8ea164d47388472001
Kortique <40215395+Kortique@users.noreply.github.com>
2021-09-09 12:44:59 +0300

    Made refactoring in Kotlin style

--- app/src/main/java/com/example/movieapp/utils/Extensions.kt
+++ app/src/main/java/com/example/movieapp/utils/Extensions.kt
@@ -1,12 +1,29 @@
 package com.example.movieapp.utils
 
+import android.view.View
+import androidx.annotation.StringRes
+import com.google.android.material.snackbar.Snackbar
 import java.text.SimpleDateFormat
 import java.util.*
 
 fun String.toDate(format: String, locale: Locale = Locale.getDefault()) =
     SimpleDateFormat(format, locale).parse(this)!!
-
 fun Date.toString(format: String, locale: Locale = Locale.getDefault()): String {
     val formatter = SimpleDateFormat(format, locale)
     return formatter.format(this)
+}
+
+fun View.showSnackBar(
+    @StringRes stringId: Int,
+    duration: Int = Snackbar.LENGTH_SHORT,
+    @StringRes actionStringId: Int? = null,
+    action: View.OnClickListener? = null
+) {
+    val snackBar = Snackbar.make(this, stringId, duration)
+
+    if (actionStringId != null && action != null) {
+        snackBar.setAction(actionStringId, action)
+    }
+
+    snackBar.show()
 }
