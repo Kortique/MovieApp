f2bb8bc4ee8171f929144af6df1cbe48f2e6fb62
Kortique <40215395+Kortique@users.noreply.github.com>
2021-09-20 14:41:13 +0300

    Created a new service for downloading info from https://www.themoviedb.org.

--- app/src/main/java/com/example/movieapp/receivers/ConnectivityActionBroadcastReceiver.kt
+++ app/src/main/java/com/example/movieapp/receivers/ConnectivityActionBroadcastReceiver.kt
@@ -3,7 +3,7 @@ package com.example.movieapp.receivers
 import android.content.BroadcastReceiver
 import android.content.Context
 import android.content.Intent
-import com.example.movieapp.moviedb.R
+import com.example.movieapp.R
 import com.example.movieapp.utils.showToast
 
 class ConnectivityActionBroadcastReceiver : BroadcastReceiver() {
