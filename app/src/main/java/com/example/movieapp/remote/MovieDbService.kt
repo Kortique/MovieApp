e705078b2f6f1c766fc490c6976290fee28e1df6
Kortique <40215395+Kortique@users.noreply.github.com>
2021-09-28 17:52:20 +0300

    Repair access to TMDB

--- app/src/main/java/com/example/movieapp/remote/MovieDbService.kt
+++ app/src/main/java/com/example/movieapp/remote/MovieDbService.kt
@@ -1,5 +1,6 @@
 package com.example.movieapp.remote
 
+import com.example.movieapp.BuildConfig
 import com.example.movieapp.remote.entites.MoviesDTO
 import retrofit2.Call
 import retrofit2.http.GET
@@ -9,12 +10,14 @@ interface MovieDbService {
 
     @GET("3/movie/now_playing")
     fun getNowPlayingMovies(
+        @Query("api_key") key: String = BuildConfig.MOVIEDB_API_KEY,
         @Query("language") language: String = "en-US",
         @Query("page") page: Int = 1
     ): Call<MoviesDTO>
 
     @GET("3/movie/upcoming")
     fun getUpcomingMovies(
+        @Query("api_key") key: String = BuildConfig.MOVIEDB_API_KEY,
         @Query("language") language: String = "en-US",
         @Query("page") page: Int = 1
     ): Call<MoviesDTO>
